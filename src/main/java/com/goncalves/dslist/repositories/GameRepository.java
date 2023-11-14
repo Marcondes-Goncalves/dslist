96748aa98b363a72d8fa8552ce1bb7019a3512e5
Marcondes-Goncalves <marcondesgoncalves159@gmail.com>
2023-11-13 22:39:29 -0300

    Game repository, service, controller

---
+++ src/main/java/com/goncalves/dslist/repositories/GameRepository.java
@@ -0,0 +1,14 @@
+package com.goncalves.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.goncalves.dslist.entities.Games;
+
+public interface GameRepository extends JpaRepository<Games, Long> {
+    
+
+
+
+
+
+}
---
+++ src/main/java/com/goncalves/dslist/services/GameService.java
@@ -0,0 +1,29 @@
+package com.goncalves.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.goncalves.dslist.dto.GameMinDTO;
+import com.goncalves.dslist.entities.Games;
+import com.goncalves.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+    
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll(){
+        List<Games> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+        
+    }
+
+
+
+
+
+}
